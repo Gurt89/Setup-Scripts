e4829153bc388c1283409d6236fe3362619111bc
Nick Gurtler <gurt89@gmail.com>
2018-07-12 16:55:47 -0500

    First Try

---
+++ JoinDomain_Brkn.ps1
@@ -0,0 +1,9 @@
+﻿$newName = Read-Host -Prompt "Enter New Computer Name"
+$domain = "gurtler.local"
+$user = "ngurtler"
+$password = Read-Host -Prompt "Enter password for $user" -AsSecureString 
+$username = "$domain\$user" 
+$credential = New-Object System.Management.Automation.PSCredential($username,$password) 
+Rename-Computer -NewName $newName -Force
+Write-Host "Please waiting for a moment to change Domain and then restart" -ForegroundColor Red
+Add-Computer -DomainName $domain -Options JoinWithNewName -Credential $credential
---
+++ MAC_NAME.ps1
@@ -0,0 +1,12 @@
+﻿$macAddresses = Get-WmiObject win32_networkadapterconfiguration
+$tab=$macAddresses.macaddress
+$netbiosName=$(Get-WmiObject Win32_Computersystem).name
+$array = New-Object System.Collections.ArrayList
+foreach ($mc in ($tab | Where-Object {$_ -ne $null}))
+{
+ $array.add([PSCUSTOMOBJECT]@{
+    'Mac' = $mc
+    'Netbiosname' = $netbiosName})
+     
+}
+$array | Export-Csv H:\result.txt -notypeinformation -encoding UTF8
